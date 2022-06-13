838f6bb640ff254085ad10e4ad8f3b8eef7278f6
Xin Zhong <1140091006@qq.com>
2022-06-13 12:25:17 +0800

    more mode

--- app.py
+++ app.py
@@ -3,11 +3,38 @@ import PySimpleGUI as sg
 import random
 from subject import dict
 
+
+sg.theme('GreenMono')
+
+layout = [
+    [sg.Text("这是HDU中国近代史客观题刷题app", size=(64, 2), font=20, text_color='Black', auto_size_text=True)],
+    [sg.Text("由于开发者的技术水平较弱且极为懒惰", size=(64, 2), font=20, text_color='Black')],
+    [sg.Text("只能设计出如此简陋敷衍的UI界面", size=(64, 2), font=20, text_color='Black')],
+    [sg.Text("相信大家最在乎的不是这个所谓的皮囊而且接下来有趣的灵魂", size=(64, 2), font=20, text_color='Black')],
+    [sg.Text("那请选择模式吧", size=(64, 2), font=20, text_color='Black')],
+    [sg.Text("                                                 ", size=(64, 1), font=10, text_color='Red')],
+    [sg.Button('  顺序模式  ', key="1", font=20), sg.Button('  随机模式  ', key="2", font=20),sg.Button('  退出程序  ',key='3',font=20)],
+    [sg.Text("                                                                                                        By HDU_钟鑫")]]
+window = sg.Window('近代史题库', layout, default_element_size=(24, 2), auto_size_text=True)
+event, values = window.read()
+window.close()
+# 模式
+if event =='1':
+    mode = 'linear'
+elif event=='2':
+    mode = 'random'
+else:
+    exit(0)
 count = 0
 acc = 0
-sg.theme('GreenMono')
+i=-1
 while True:
-    i = random.randint(0,248)
+    if mode == 'random':
+        i = random.randint(0, 248)
+    else:
+        i += 1
+        if i > 248:
+            break
     layout = [
         [sg.Text(dict[i]['tm'], size=(64, 3), font=20, text_color='Black', auto_size_text=True)],
         [sg.Text(dict[i]['xs'], size=(64, 5), font=20, text_color='navy')],
